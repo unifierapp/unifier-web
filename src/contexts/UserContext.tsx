0de4466f3993216d017a563c0c68c60ad40d0a9f
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-24 12:15:44 +0700

    feat: User context

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,15 +1,26 @@
+"use client";
 import React from "react";
-
+import api from "@/helpers/api";
 
 export const UserContext = React.createContext<{ user: IUser | null, refresh: () => void }>({
     user: null,
-    refresh() {
-    }
+    refresh() {},
 });
 
 export function UserWrapper(props: React.PropsWithChildren<{}>) {
     const [loaded, setLoaded] = React.useState(false);
-    const [user, setUser] = React.useState(null);
+    const [user, setUser] = React.useState<IUser | null>(null);
+
+    React.useEffect(() => {
+        if (!loaded) {
+            load().then();
+        }
+    }, [loaded]);
+
+    async function load() {
+        const user = (await api.get<IUser | null>("/users/current")).data;
+        setUser(user);
+    }
 
     function refresh() {
         setLoaded(false);
@@ -17,6 +28,6 @@ export function UserWrapper(props: React.PropsWithChildren<{}>) {
 
     return <UserContext.Provider value={{
         refresh,
-        user: null
+        user,
     }}></UserContext.Provider>
 }
--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,8 @@
 declare interface IUser {
-    email: string,
-    username: string,
-    profilePictureUrl: string,
-    profilePictureCloudId: string,
-    onboarded: boolean,
-    _id: string,
+    _id: string;
+    username: string;
+    email: string;
+    profilePictureUrl: string;
+    profilePictureCloudId?: string;
+    onboarded: boolean;
 }
